cdc34b0e32de211501058321eb59e148ece6a6b0
Aadithya Vasudevan <68108339+Aadithya-19@users.noreply.github.com>
2021-02-23 14:34:10 +0530

    Delete scatter_plot.py

--- scatter_plot.py
+++
@@ -1,8 +0,0 @@
-import plotly.express as ps
-import pandas as spb
-
-data = spb.read_csv("data.csv")
-
-fig = ps.scatter(data, x = "date", y = "cases", color = "country", size_max = 60, title = "Internet users Per country")
-
-fig.show()  
